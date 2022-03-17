7ee67e1b0e67de30534c6c6f2c19836b8d144fbc
Jason Yuill <j.yuill828@gmail.com>
2022-03-17 10:03:05 -0600

    updated python script

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+for x in range(10):
+    print("ype this works")
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+print("here is some text that I want to print")
 
+x = 8
+y = 2
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def trythisout(x: int, y:int):
+    out = x ** y
+    print(out)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+trythisout(x, y)
