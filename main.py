4961d48fcacf374ae76895cd65df32fdddbe2dac
Jason Yuill <j.yuill828@gmail.com>
2022-03-17 14:32:03 -0600

    updating the main file to show the open py xl basics

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
-# Here is the start of my python file
+"""The start of my openpyxl learning journey. The first thing I did was make sure to download
+the openpyxl module. I did this through pycharm, so I believe it is in the virtual environment.
+Next, I will be working through the openpyxl tutorial from the .io readthe docs webpage."""
+from openpyxl import Workbook
 
-x, y = 8, 33
+# Here we will create the workbook class (Technically, wb is an instance of the Workbook class)
+# The workbook class uses () because there are no needed input parameters
+wb = Workbook()
 
-for letter in range(10):
-    print(x + y)
+# when a workbook is created, it is alwasys made with at least one worksheet. Lets acces it!:
+ws = wb.active
 
+# great! now lets make another worksheet! (very easy)
+ws2 = wb.create_sheet("mynewsheet")
+
+# what if we don't like the name of a worksheet? well just change it!
+ws.title = "new title"
